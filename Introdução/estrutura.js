ffb152f119736fce8eb142217614c42a40fdd819
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-09 23:31:33 -0300

    Introdução - Estrutura de repetição 2

--- Introdução/estrutura.js
+++ Introdução/estrutura.js
@@ -2,7 +2,7 @@
 const alunosTurmaA = [
     {
         nome: "João César",
-        nota: 7.0
+        nota: 10
     },
     {
         nome: "Maria Paula",
@@ -11,6 +11,10 @@ const alunosTurmaA = [
     {
         nome: "Paula Cristina",
         nota: 10
+    },
+    {
+        nome: "Antônio Roberto",
+        nota: 9.3
     }
 ]
 
@@ -35,14 +39,18 @@ const alunosTurmaB = [
 ]
 
 function calculaMedia(alunos){
+    let soma = 0;
     for (let i = 0; i < alunos.lenght; i++){
-        console.log(i);
+        soma = soma + alunos[i].nota        
     }
+
+    const media = soma / alunos.lenght;
+    return media;
 }
 
 
 const media1 = calculaMedia(alunosTurmaA);
-// const media2 = calculaMedia(alunosTurmaB);
+const media2 = calculaMedia(alunosTurmaB);
 
 function enviaMensagem(media, turma){
     if (media > 6){
@@ -52,4 +60,9 @@ function enviaMensagem(media, turma){
         console.log(`A média da ${turma} foi menor que 6. Reprovado.`)
     }
 }
+
+enviaMensagem(media1.toFixed(2), 'Turma A');
+enviaMensagem(media2.toFixed(2), 'Turma B');
+
+console.log()
 
