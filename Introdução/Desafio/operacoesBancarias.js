7ed238d7ab121e09c2bb4f5a2fa5f791e771c4bd
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-10 20:48:29 -0300

    Introdução - Desafio Operações Bancárias 2

--- Introdução/Desafio/operacoesBancarias.js
+++ Introdução/Desafio/operacoesBancarias.js
@@ -1,11 +1,34 @@
 // Criar um programa que execute atividades bancárias padrões.
 
-const usuario = {
-    nome: "Mariana",
-    transacoes = [],
-    saldo: 0
+const user = {
+    name: 'Mariana',
+    transactions: [],
+    balance: 0
 }
 
-function deposito(){
-    
+
+function createTransaction(transaction) {
+    user.transactions.push(transaction)
+
+    if (transaction.type === 'credit') {
+        user.balance = user.balance + transaction.value
+    } else {
+        user.balance = user.balance - transaction.value
+    }
 }
+
+
+function getHigherTransactionByType(type) {
+    let higherTransaction
+    let higherValue = 0
+
+    for (let transaction of user.transactions) {
+        if (transaction.type == type && transaction.value > higherValue) {
+            higherValue = transaction.value
+            higherTransaction = transaction
+        }
+    }
+
+    return higherTransaction
+}
+
