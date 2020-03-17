0eb9ec78c1c576e3fb57810f8a4ca114c15bad51
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-16 21:05:58 -0300

    Introdução - Página WEB - finalizando parte inicial

--- Introdução/PaginasWeb/scripts.js
+++ Introdução/PaginasWeb/scripts.js
@@ -4,8 +4,9 @@ const cards = document.querySelectorAll('.card');
 
 for (let card of cards){
     card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active')
-        modalOverlay.querySelector("iframe").src=`https://www.youtube.com/embed/${videoID}`;
+        modalOverlay.querySelector("iframe").src=`https://www.youtube.com/embed/${videoId}`;
 
 
     })
@@ -13,6 +14,8 @@ for (let card of cards){
 
 document.querySelector('.close-modal').addEventListener("click", function(){
     modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src="";
+    
 })
 
 
