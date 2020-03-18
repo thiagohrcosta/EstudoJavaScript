443c7640f09230926123b74598e6020b9c104096
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-17 23:52:15 -0300

    Pequeno testes aleatórios de códigos

--- Introdução/objetos.js
+++ Introdução/objetos.js
@@ -16,3 +16,18 @@ const aluno03 = {
 const media = (aluno01.nota + aluno02.nota + aluno03.nota) / 3;
 const notaMedia = media.toFixed(2);
 console.log(`Nota média da turma foi de: ${notaMedia}` );
+
+console.log(aluno01.nome, aluno02.nota)
+
+/*
+inteiro ano,idade,nascimento
+ano=2019
+idade=40
+nascimento = ano - idade
+escreva(“Seu ano de nascimento é: “ + nascimento) */
+
+const ano = 2019
+const idade = 40
+const nascimento = ano - idade;
+
+console.log("Seu ano de nascimento e: " + nascimento);
