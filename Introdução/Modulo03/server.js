097e16993454d2e9ee010c5b3e1728cc6cea6336
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-19 20:23:14 -0300

    Módulo 3 - Instalando nunjucks

--- Introdução/Modulo03/server.js
+++ Introdução/Modulo03/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const nunjucks = require("nunjucks")
+
 const server = express();
 
+server.set("view engine", "html")
+
+server.use(express.static("public"))
+
+nunjucks.configure("views", {
+    express: server
+})
+
 server.get("/", function(req, res){
-    return res.send("Olá! Usando NODEMON")
-} )
+    return res.render("index")
+})
 
 server.listen(5000, function(){
     console.log("Server is running on port 5000.")
