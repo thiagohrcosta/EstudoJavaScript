183947978438012df46aa22b7dad3f5907288604
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-31 16:12:40 -0300

    Módulo 04 - Academia - rota create

--- Introdução/Academia/routes.js
+++ Introdução/Academia/routes.js
@@ -9,6 +9,10 @@ routes.get("/instructors", function(req, res){
     return res.render("instructors/index")
 })
 
+routes.get("/instructors/create", function(req, res){
+    return res.render('instructors/create')
+})
+
 routes.get("/members", function(req, res){
     return res.send("members")
 })
