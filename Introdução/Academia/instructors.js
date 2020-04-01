cad3097bf00bf5eefe24ea4c1452c61533f14211
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-01 15:46:19 -0300

    Módulo - 04 - Academia - Apresentando os dados

--- Introdução/Academia/instructors.js
+++ Introdução/Academia/instructors.js
@@ -1,6 +1,31 @@
 const fs = require("fs");
 const data = require("./data.json");
 
+// Show
+
+exports.show = function(req, res){
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id;
+    })
+
+    if (!foundInstructor) return res.send("Instrutor não localizado");
+
+    const instructor = {
+        ...foundInstructor,
+        age: "",
+        gender: "",
+        services: "",
+        created_at: ""
+    }
+
+    return res.render("instructors/show", { instructor : foundInstructor});
+
+}
+
+
+// Create
 exports.post = function(req, res){
 
         const keys = Object.keys(req.body)
