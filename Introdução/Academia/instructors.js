60f1b7b62680a79d38ffe54a7efbb34a58d6949c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-01 15:00:07 -0300

    Módulo 04 - Academia - Tratamento de dados e destructuring

--- Introdução/Academia/instructors.js
+++ Introdução/Academia/instructors.js
@@ -1,26 +1,32 @@
 const fs = require("fs");
 const data = require("./data.json");
 
-// create
-
 exports.post = function(req, res){
-        // req.query
-        // req.body => em decorrência do post
-    
+
         const keys = Object.keys(req.body)
     
         for ( key of keys ) {
-            // req.body.avatar_url  = res.body[key]
             if (req.body[key] == ""){
                 return res.send("Por favor, preencha todos os campos!")
             }
         }
 
-        req.body.birth = Date.parse(req.body.birth);
-        req.body.created_at = Date.now();
+        let {avatar_url, birth, name, services, gender} = req.body
 
-        // []
-        data.instructors.push(req.body);  // [{...}]
+        birth = Date.parse(birth);
+        const created_at = Date.now();
+        const id = Number(data.instructors.length + 1);
+
+
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services,
+            created_at,
+        });
 
         fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
             if (err) return res.send("Write file error!")
