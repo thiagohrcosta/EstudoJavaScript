22b1c546ca8d591152dc0d57f7523639cc988c0f
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-03 18:58:36 -0300

    Módulo 04 - Academia - Configurando lógica do menu ativo e bugs

--- Introdução/Academia/public/scripts.js
+++ Introdução/Academia/public/scripts.js
@@ -1,1 +1,11 @@
-const currentPage = location.pathname
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll("header .links a")
+
+
+for (item of menuItems){
+    if (currentPage.includes(item.getAttribute("href"))){
+        item.classList.add("active")
+        
+    }
+}
+
