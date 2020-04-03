535fef34d5fd23494ee6384d06adafbce911e9de
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-03 17:13:22 -0300

    Módulo 04 - Academia - estilizando tabela

--- Introdução/Academia/instructors.js
+++ Introdução/Academia/instructors.js
@@ -89,9 +89,13 @@ exports.edit = function(req, res){
 
 exports.put = function(req, res){
     const { id } = req.body
+    let index = 0;
 
-    const foundInstructor = data.instructors.find(function(instructor){
-        return id == instructor.id
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if ( id == instructor.id ) {
+            index = foundIndex
+            return true
+        }
     })
 
     if (!foundInstructor) return res.send("Instrutor não localizado")
@@ -99,10 +103,11 @@ exports.put = function(req, res){
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
-    data.instructors[id -1] = instructor
+    data.instructors[index] = instructor
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("Erro ao salvar!")
