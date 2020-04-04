b3ce2e53647aa8f0aa089c810750831f0498d172
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-04 07:10:30 -0300

    Módulo 04 - Academia - Salvando e apresentando dados para os Membros

--- Introdução/Academia/controllers/members.js
+++ Introdução/Academia/controllers/members.js
@@ -38,32 +38,28 @@ exports.post = function(req, res){
             }
         }
 
-        let {avatar_url, birth, name, services, gender} = req.body
+        birth = Date.parse(req.body.birth);
 
-        birth = Date.parse(birth);
-        const created_at = Date.now();
-        const id = Number(data.members.length + 1);
+        let id = 1;
+        const lastMember = data.members[data.members.length - 1];
 
+        if (lastMember){
+            id = lastMember.id + 1;
+        }
 
         data.members.push({
             id,
-            avatar_url,
-            name,
-            birth,
-            gender,
-            services,
-            created_at,
+            ...req.body,
+            birth 
         });
 
         fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
             if (err) return res.send("Write file error!")
 
-            return res.redirect("/members");
+            return res.redirect(`/members/${id}`);
 
         })
-    
-        // return res.send(req.body)
-    
+        
 }
 
 exports.edit = function(req, res){
