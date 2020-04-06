691788a79693bee43edbda8af5ac7f7c1977886e
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-06 17:06:53 -0300

    Módulo 05 - Academia - Ajuste nos membros e ordenando lista

--- Introdução/Modulo05/Academia/src/app/controllers/members.js
+++ Introdução/Modulo05/Academia/src/app/controllers/members.js
@@ -1,8 +1,15 @@
 const { age, date } = require("../../lib/utils");
 
+
+const Member = require("../models/Member");
+
 module.exports = {
     index(req, res){
-        return res.render("members/index")
+
+        Member.all(function(members){
+            return res.render("members/index", { members })
+
+        })       
  
     },
     create(req, res){
@@ -17,16 +24,31 @@ module.exports = {
                 return res.send("Por favor, preencha todos os campos!")
             }
         }
-       
-        return
+
+        Member.create(req.body, function(member){
+            return res.redirect(`/members/${member.id}`)
+        })
+        
     
     },
     show(req, res){
-        return
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Member not found!")
+            
+            member.birth = date(member.birth).birthDay
+
+            return res.render("members/show", { member })
+        })
 
     },
     edit(req, res){
-        return
+        Member.find(req.params.id, function(member){
+            if(!member) return res.send("Member not found!")
+            
+            member.birth = date(member.birth).iso
+            
+            return res.render("members/edit", { member })
+        })
 
     },
     put(req, res){
@@ -38,12 +60,21 @@ module.exports = {
             }
         }
 
-        return
+        Member.update(req.body, function(){
+            return res.redirect(`/members/${req.body.id}`)
+        })
+
+        
 
     },
     delete(req, res){
-        return
+        Member.delete(req.body, function(){
+            return res.redirect(`/members/${req.body.id}`)
+        })
 
     },
+
+
+        
 
 }
--- Introdução/Modulo05/Academia/src/config/db.js
+++ Introdução/Modulo05/Academia/src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg")
 
 module.exports = new Pool({
     user: 'postgres',
-    password: "",
+    password: "123456postgre",
     host: "localhost",
     port: 5432,
     database: "gymmanager"
