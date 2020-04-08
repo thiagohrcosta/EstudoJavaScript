f32b8a28fc19b5bebb40c9e7acd2d8ef85e4c383
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-08 11:46:10 -0300

    Módulo 05 - Academia - Lógica de paginação

--- Introdução/Modulo05/Academia/src/app/controllers/instructors.js
+++ Introdução/Modulo05/Academia/src/app/controllers/instructors.js
@@ -4,7 +4,28 @@ const Instructor = require("../models/Instructor");
 module.exports = {
     index(req, res){
 
-        const { filter } = req.query
+        let { filter, page, limit } = req.query
+
+        page = page || 1
+        limit = limit || 2
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(instructors){
+                return res.render("instructors/index", { instructors, filter })
+            }
+        }
+
+        Instructor.paginate(params)
+
+
+
+
+        /*
         if (filter) {
             Instructor.findBy(filter, function(instructors){
                 return res.render("instructors/index", { instructors, filter })
@@ -17,7 +38,7 @@ module.exports = {
 
         })
     }
-       
+       */
  
     },
     create(req, res){
