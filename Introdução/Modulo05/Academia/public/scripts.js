9036ed796645d6a55ff91d1242bfc851e819a316
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-08 12:02:51 -0300

    Módulo 05 - Academia - Algoritmo de paginação no FrontEnd

--- Introdução/Modulo05/Academia/public/scripts.js
+++ Introdução/Modulo05/Academia/public/scripts.js
@@ -9,3 +9,34 @@ for (item of menuItems){
     }
 }
 
+// Paginação
+
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldPage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+    pages.push(currentPage)
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPages = currentPage <= selectedPage + 2 
+    const pagesBeforeSelectedPages = currentPage >= selectedPage - 2
+
+    if (firstAndLastPage || pagesBeforeSelectedPages && pagesAfterSelectedPages){
+        
+        if(oldPage && currentPage - oldPage > 2){
+            pages.push("...")    
+        }
+
+        if (oldPage && currentPage - oldPage == 2){
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+
+        oldPage = currentPage
+        
+    }
+}
+
