36ff1fcefc21f996b2c9c5ee0e757105437f922c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-08 14:09:06 -0300

    Módulo 05 - Academia - adicionando paginate

--- Introdução/Modulo05/Academia/src/app/controllers/members.js
+++ Introdução/Modulo05/Academia/src/app/controllers/members.js
@@ -1,15 +1,34 @@
 const { age, date } = require("../../lib/utils");
 
-
 const Member = require("../models/Member");
 
 module.exports = {
     index(req, res){
 
-        Member.all(function(members){
-            return res.render("members/index", { members })
+        let { filter, page, limit } = req.query;
 
-        })       
+        page = page || 1;
+        limit = limit || 2;
+        let offset = limit * (page - 1);
+    
+        const params = {
+          filter,
+          page,
+          limit,
+          offset,
+          callback(members) {
+            const pagination = {
+              total: Math.ceil(members[0].total / limit),
+              page,
+            };
+            return res.render("members/index", {
+              members,
+              pagination,
+              filter,
+            });
+          },
+        };
+        Member.paginate(params);  
  
     },
     create(req, res){
@@ -78,6 +97,40 @@ module.exports = {
         })
 
     },
+
+    paginate(params) {
+        const { filter, limit, offset, callback } = params;
+    
+        let query = "",
+          filterQuery = "",
+          totalQuery = `(
+                    SELECT count(*) FROM members
+                ) AS total`;
+    
+        if (filter) {
+          filterQuery = `
+                WHERE members.name ILIKE '%${filter}%'
+                OR members.email ILIKE '%${filter}%'
+                `;
+    
+          totalQuery = `(
+                    SELECT count(*) FROM members
+                    ${filterQuery}
+                ) as total`;
+        }
+    
+        query =`
+            SELECTmemberss.*, ${totalQuery}
+            FROM instructors
+            ${filterQuery}
+            LIMIT $1 OFFSET $2`
+    
+            db.query(query, [limit, offset], function(err, results){
+                if (err) throw 'Database Error!'
+                
+                callback(results.rows)
+        });
+      },
 
 
         
